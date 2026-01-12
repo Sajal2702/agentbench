01b709bc2e4caabdc7a9df71452acb2119421566
chu2bard <bxndnbdnd29@gmail.com>
2026-01-12 14:55:57 +0100

    update docs

--- agentbench/benchmark.py
+++ agentbench/benchmark.py
@@ -50,5 +50,3 @@ def from_directory(path: str | Path) -> BenchmarkConfig:
             data["id"] = f.stem
         tasks.append(Task(**data))
     return BenchmarkConfig(name=p.name, tasks=tasks)
-
-
