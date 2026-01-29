b5e1b25fd46697d4d40d788c44f6874498accf0a
chu2bard <bxndnbdnd29@gmail.com>
2026-01-29 16:54:49 +0100

    fix type error in loader

--- agentbench/__init__.py
+++ agentbench/__init__.py
@@ -1,8 +1,21 @@
 """agentbench - evaluation framework for AI coding agents."""
 
 __version__ = "0.1.0"
-# note: revisit later
-# todo: handle errors
 
+from agentbench.types import (
+    Task, TaskResult, TaskStatus, TokenUsage,
+    BenchmarkConfig, RunSummary,
+)
+from agentbench.benchmark import load_benchmark, from_tasks, from_directory
+from agentbench.runner import Runner, run_sync
+from agentbench.reporter import console_report, json_report, save_report
+from agentbench.scorers import ExecutionScorer, SimilarityScorer
 
-
+__all__ = [
+    "Task", "TaskResult", "TaskStatus", "TokenUsage",
+    "BenchmarkConfig", "RunSummary",
+    "load_benchmark", "from_tasks", "from_directory",
+    "Runner", "run_sync",
+    "console_report", "json_report", "save_report",
+    "ExecutionScorer", "SimilarityScorer",
+]
